e0709ccbbf878216768a384539073ccabfb03a5f
Xian Fu Hui <73300114+xianfuhui@users.noreply.github.com>
2026-08-05 00:00:06 +0700

    emulator

---
+++ mobile/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,23 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
+#include <flutter_tts/flutter_tts_plugin.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
+#include <vosk_flutter/vosk_flutter_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
+  FlutterTtsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterTtsPlugin"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+  VoskFlutterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("VoskFlutterPlugin"));
+}
